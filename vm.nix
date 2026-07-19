edd80429006bd23bafdd74af150ad8d65e8a27f8
Dane <58756948+dane-io@users.noreply.github.com>
2026-07-18 22:33:56 -0500

    vm.nix: added some bootloader stuff

--- vm.nix
+++ vm.nix
@@ -3,4 +3,8 @@
 {
   imports = [ ./configuration.nix ];
   networking.hostName = "radio-vm";
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 }
